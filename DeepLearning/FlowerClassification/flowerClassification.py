4db85fcdeae0ac26940c62ba07ac932f648a46f1
Cody <meanderingmonarch@gmail.com>
2016-11-02 16:16:05 -0500

    updating iris classifier

--- DeepLearning/FlowerClassification/flowerClassification.py
+++ DeepLearning/FlowerClassification/flowerClassification.py
@@ -9,3 +9,17 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import pipeline
+
+#fix random seed
+seed = 7
+numpy.random.seed(seed)
+
+#load dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+raw_data = urllib.urlopen(url)
+dataSet = numpy.loadtxt(raw_data, delimiter=",")
+#lets split the data into the 8 input variables and then assign a single output
+X = dataSet[:,0:4].astype(float)
+Y = dataSet[:,4]
+
+#encode class values as integers
