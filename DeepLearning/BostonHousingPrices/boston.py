790eb3fceb333744e8ada050d867589e78cfd0f5
Cody <meanderingmonarch@gmail.com>
2016-11-13 14:14:31 -0600

    updated boston housing prices scenario

--- DeepLearning/BostonHousingPrices/boston.py
+++ DeepLearning/BostonHousingPrices/boston.py
@@ -1,2 +1,12 @@
 import numpy
 import pandas
+# https://keras.io/models/sequential/
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+#load data sets
