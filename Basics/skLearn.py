298a484cfc94126102ee5025faca7a3ce42262eb
Cody <meanderingmonarch@gmail.com>
2016-11-14 08:19:28 -0600

    Added SKLEARN basics piece

---
+++ Basics/skLearn.py
@@ -0,0 +1,9 @@
+
+
+
+# To see your accuracy
+#Take the data you want to predict and compare to the end result labels
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(prediction, labels_test)
+
+print acc
