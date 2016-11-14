6586b6a47cb582adee407b108aa761881b146a71
Cody <meanderingmonarch@gmail.com>
2016-11-14 09:10:22 -0600

    iris examle updated

--- Basics/iris.py
+++ Basics/iris.py
@@ -1,6 +1,7 @@
 import numpy as np
 #Decision trees are easy to visualize
 from sklearn.datasets import load_iris
+from sklearn import tree
 #sklearn has slick adoptions of classic machine learning data sets
 iris = load_iris()
 print iris.feature_names
@@ -14,3 +15,16 @@ print iris.target[0]
 test_iris = [0, 50, 100]
 
 #set u the training data
+#remove test data from training dataset
+train_target = np.delete(iris.target, text_iris)
+#establish training data
+train_data = np.delete(iris.data, text_iris, axis=0)
+
+#testing data, just the three examples above
+test_target = iris.target[test_iris]
+test_data = iris.data[test_iris]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print test_target
