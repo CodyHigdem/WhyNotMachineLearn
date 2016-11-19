332eef873a599723b736351b6c2e3521c6c7dc92
Cody <meanderingmonarch@gmail.com>
2016-11-18 21:33:01 -0600

    updated boston pricing example

--- DeepLearning/BostonHousingPrices/boston.py
+++ DeepLearning/BostonHousingPrices/boston.py
@@ -16,3 +16,27 @@ dataset = dataframe.values
 X = dataset[:,0:13]
 Y = dataset[:,13]
 
+#define base mode
+#ADAM
+#rectifier activation is considered a 'best practice' with neural nets
+
+def baseline_model():
+	#create model
+	model = Sequential();
+	model.add(Dense(13, input_dim=13, init="normal", activation="relu"))
+	model.add(Dense(1, init="normal"))
+	#compile model
+	model.compile(loss="mean_squared_error", optimizer="adam")
+	return model
+
+#fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+#evaluate model with standardized dataset
+estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=5, verbose=0)
+
+kfold = KFold(n=len(X), n_folds=10, random_state=seed)
+results = cross_val_score(estimator, X, Y, cv=kfold)
+print("Baseline: %.2f (%.2f) MSE " % (results.mean(), results.std()))
+
+
